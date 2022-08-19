13f323acfabb8471dc72f11022c6488ab7e55887
Jay Lin <anton850117ma@gmail.com>
2022-08-19 10:34:18 +0800

    update: still cannot fix dll problem

--- make.bat
+++ make.bat
@@ -6,6 +6,7 @@ if exist "autotrade" rmdir /s /q "autotrade
 if exist "autotrade.zip" del "autotrade.zip"
 
 cmake -G "Visual Studio 17 2022" -A x64 -S . -B build
+@REM cmake -G "Visual Studio 17 2022" -A Win32 -S . -B build
 cmake --build build --target ALL_BUILD --config Release --parallel
 
 zip -r -q autotrade.zip autotrade/
